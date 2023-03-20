c5e1a067294e1c7320964f6b1a46a7b33b727192
Maximo <pachecomaximo11@gmail.com>
2023-03-19 22:28:25 -0300

    nextapi variable

--- api/config.ts
+++ api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const forUApi = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000/api'
 })
 
 export default forUApi
